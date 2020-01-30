a862a3accebdc065abde8ce0e6068fcf4768223c
kagiso <kagisowilliamdev@gmail.com>
2020-01-30 22:23:38 +0200

    Navigation design is implemented accordingly

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatBadgeModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatBadgeModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatTabsModule } from '@angular/material';
 
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
-    exports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule]
+    imports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatTabsModule],
+    exports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatTabsModule]
 })
 
 export class MaterialModule {}
